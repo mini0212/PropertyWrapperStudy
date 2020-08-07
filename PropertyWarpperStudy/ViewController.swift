79e4b6c50e1fe6236adb268dfe4cf0721bf900f6
min_eee <seongmin.kim@flitto.com>
2020-08-07 15:20:03 +0900

    update: UI 추가

--- PropertyWarpperStudy/ViewController.swift
+++ PropertyWarpperStudy/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private(set) var nameTextField: UITextField!
+    @IBOutlet private(set) var phoneTextField: UITextField!
+    @IBOutlet private(set) var saveButton: UIButton!
+    @IBOutlet private(set) var showButton: UIButton!
+    @IBOutlet private(set) var nameLabel: UILabel!
+    @IBOutlet private(set) var phoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        let userInfo = UserInfo(name: nameTextField.text,
+                                phone: phoneTextField.text)
+        print(userInfo)
+        
+    }
+    
+    @IBAction func showButtonClick(_ sender: UIButton) {
+    
+    }
+    
 }
 
